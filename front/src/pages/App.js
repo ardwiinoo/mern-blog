7d39fbed305bda3dacafc4a80e064c360a60ec26
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-02-15 19:24:19 +0700

    init

---
+++ front/src/pages/App.js
@@ -0,0 +1,8 @@
+import { Router } from "../config";
+import "./App.css";
+
+function App() {
+  return <Router />;
+}
+
+export default App;
---
+++ front/src/pages/Register/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <div>Ilustrasi</div>
+      <div>Form Register</div>
+    </div>
+  );
+}
+
+export default Register;
