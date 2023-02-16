af983cd3d5085ec0b662ec4f28556169e30e7c8a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-02-16 19:22:51 +0700

    modif: penyesuaian jsx dan styling

--- front/src/pages/MainApp/index.js
+++ front/src/pages/MainApp/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Footer, Header } from "../../components";
 import CreateBlog from "../CreateBlog";
 import DetailBlog from "../DetailBlog";
 import Home from "../Home";
+import "./mainApp.scss";
 
 function MainApp() {
   return (
-    <div>
-      <p>Header</p>
-      <Routes>
-        <Route path="/create-blog" element={<CreateBlog />} />
-        <Route path="/detail-blog" element={<DetailBlog />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <p>Footer</p>
+    <div className="main-app-wrapper">
+      <Header />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path="/create-blog" element={<CreateBlog />} />
+          <Route path="/detail-blog" element={<DetailBlog />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
