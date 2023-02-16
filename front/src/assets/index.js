fa50ddac9d69c19d7f79fa13ed978a2e269c0676
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-02-16 19:20:54 +0700

    modif: menambah import export logo mongo dan icons

--- front/src/assets/index.js
+++ front/src/assets/index.js
@@ -1,4 +1,10 @@
 import registerBg from "./images/blog.jpg";
 import loginBg from "./images/blog.jpg";
+import logoMongoDB from "./images/mongoDB.svg";
 
-export { registerBg, loginBg };
+// ICON
+import ICGithub from "./icons/LogosGithubIcon.svg";
+import ICFacebook from "./icons/LogosFacebook.svg";
+import ICInstagram from "./icons/LogosInstagramIcon.svg";
+
+export { registerBg, loginBg, logoMongoDB, ICGithub, ICFacebook, ICInstagram };
