6695172ef394f356f397cd30daa05f73d2276042
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-02-18 18:40:01 +0700

    modif: mulai styling create blog

--- front/src/pages/CreateBlog/index.js
+++ front/src/pages/CreateBlog/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./createBlog.scss";
+import { Input, Button, Upload, TextArea, Gap } from "../../components";
 
 function CreateBlog() {
   return (
-    <div>
-      <p>CreateBlog Page</p>
+    <div className="blog-post">
+      <p className="title">Create New Blog Post</p>
+      <Input label="Post Title" />
+      <Upload />
+      <TextArea />
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title="Save" />
+      </div>
+      <Gap height={40} />
     </div>
   );
 }
