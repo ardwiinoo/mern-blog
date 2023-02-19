5a6b584388e28a8318733cc6aa22c9274d08a798
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-02-19 20:37:21 +0700

    modif: menambah button, useNavigate, dan Gap

--- front/src/components/molecules/BlogItem/index.js
+++ front/src/components/molecules/BlogItem/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { registerBg } from "../../../assets";
 import "./blogItem.scss";
+import { useNavigate } from "react-router-dom";
+import { Button, Gap } from "../../atoms";
 
 function BlogItem() {
+  const navigate = useNavigate();
+
   return (
     <div className="blog-item">
       <img src={registerBg} alt="post" className="image-thumb" />
@@ -10,6 +14,13 @@ function BlogItem() {
         <p className="title">Title Blog</p>
         <p className="author">Author - Date Post</p>
         <p className="body">lorem ipsup dolor sir amet aowkwokwokwowkowkowk</p>
+        <Gap height={20} />
+        <Button
+          title="View Detail"
+          onClick={() => {
+            navigate("/detail-blog");
+          }}
+        />
       </div>
     </div>
   );
