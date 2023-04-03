22eb0ab6dcd4db44e3f04c2a65c80859bb0fecab
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-04-03 12:02:04 +0700

    modif: setup cors, setup body parser json, setup routes auth&blog

--- back/index.js
+++ back/index.js
@@ -1,10 +1,27 @@
 import express from "express";
-import router from "./routes/api.js";
+import bodyParser from "body-parser";
+
+// routes
+import authRoutes from "./src/routes/auth.js";
+import blogRoutes from "./src/routes/blog.js";
 
 // init
 const app = express();
+app.use(bodyParser.json()); // type JSON
 
-app.use(router);
+// setup cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
 
 app.listen(5000, () => {
   console.log(`Server Running On Port 5000`);
