66b080f6ee89972cc8e6705c9f58343d53ca7223
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-19 21:25:30 +0700

    mengubah validasi error

--- back/src/controllers/blogController.js
+++ back/src/controllers/blogController.js
@@ -2,33 +2,36 @@ import { validationResult } from "express-validator";
 
 class blogController {
   static async createBlogPost(req, res, next) {
-    const { title, body } = req.body;
+    try {
+      const { title, body } = req.body;
 
-    const errors = validationResult(req);
+      const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-      console.log('err: ', errors)
-        return res.status(400).json({
-          message: "Request Error",
-          data: null
-        })
-    }
+      if(!errors.isEmpty()) {
+        const err = new Error('Invalid value');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+      }
 
-    const result = {
-      message: "Create Blog Post Success",
-      data: {
-        post_id: 1,
-        title: title,
-        // image: "imageFile.png",
-        body: body,
-        created_at: "12/06/2020",
-        author: {
-          uid: 1,
-          name: "Testing",
+      const result = {
+        message: "Create Blog Post Success",
+        data: {
+          post_id: 1,
+          title: title,
+          // image: "imageFile.png",
+          body: body,
+          created_at: "12/06/2020",
+          author: {
+            uid: 1,
+            name: "Testing",
+          },
         },
-      },
-    };
-    res.status(201).json(result);
+      };
+      res.status(201).json(result);
+    } catch(error) {
+      next(error);
+    }
   }
 }
 
